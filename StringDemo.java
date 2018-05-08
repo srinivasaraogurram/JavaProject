8a80a90cecf0b0508ef29a421b6a616d5fd5e8b6
Pallavi <tekula.pallavi@gmail.com>
2018-05-08 13:18:08 -0500

    adding String Demo.Java

---
+++ StringDemo.java
@@ -0,0 +1,32 @@
+
+public class StringDemo {
+
+	
+	public static void main(String[] args) {
+	
+		     String  s1  = "Kamalanabha" ;
+			 String s2 = "Pallavi";
+			 String s3 = s1 +" " +s2;	 
+				 
+	System.out.println("s3");
+	
+            String N1 = "Sreekari";
+            System.out.println("Sreekari");
+	
+	        String X = "Rohit";
+	        String Y = "Nandikonda";
+	        String Z = X + Y;
+	        System.out.println('Z'); 
+
+	        String P1 ="java-string -split- method";  
+	        String Result[] = P1.split("-");
+	        for(String t:Result){
+	                System.out.println(t);
+	
+String B1 ="Rohit";
+System.out.println("String length is:"+B1.length());
+	        }
+	}
+}
+
+
